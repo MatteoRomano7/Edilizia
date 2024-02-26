7cf3100ec86993c85e44ec400f99f7fa24ab816b
Matteo <ezmatteo@aol.com>
2024-02-26 15:56:52 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/DipendentiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Edilizia.Controllers
+{
+    public class DipendentiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
